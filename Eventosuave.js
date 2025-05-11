60697b1dec56d5023e8a1988b263a4ca5b972365
amilton1809 <a.milton18@hotmail.com>
2025-05-10 21:48:31 -0300

    att efeito smooth

--- Eventosuave.js
+++ Eventosuave.js
@@ -8,3 +8,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+  // Efeito Smooth no Scroll
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const elementsToAnimate = document.querySelectorAll(".projetos .projetos-card, .contatos .formulario-contato");
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, {
+      threshold: 0.6 // Ajuste para 0.2 (20%) de visibilidade para iniciar o efeito
+    });
+  
+    elementsToAnimate.forEach((element) => {
+      observer.observe(element);
+    });
+  });
+  
